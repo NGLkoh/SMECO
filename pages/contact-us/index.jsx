f3923cdbf3f80e4e56f8519657443e9bbb9c257c
NGLK <nigelkoh03@gmail.com>
2024-11-29 13:44:08 +0800

    update

--- pages/contact-us/index.jsx
+++ pages/contact-us/index.jsx
@@ -15,11 +15,13 @@ import {
   InputLeftElement,
   Textarea,
   ChakraProvider,
+  Link,
 } from '@chakra-ui/react';
 import Navbar from '../../components/nabvar';
 import Footer from '../../components/footer';
 import { BsPerson } from 'react-icons/bs';
 import { MdOutlineEmail } from 'react-icons/md';
+import { FaFacebook } from 'react-icons/fa';
 
 const ContactPage = () => {
   return (
@@ -28,7 +30,7 @@ const ContactPage = () => {
       <ChakraProvider>
         <Container
           maxW="full"
-          height="800px"
+          height="auto"
           mt={0}
           centerContent
           overflow="hidden"
@@ -42,19 +44,31 @@ const ContactPage = () => {
             borderRadius="lg"
             m={{ sm: 4, md: 16, lg: 10 }}
             p={{ sm: 5, md: 5, lg: 16 }}
+            boxShadow="md"
           >
             <Box p={4}>
               {/* Header */}
               <Box color="#000000" p={6} textAlign="center">
-                <Text fontSize="xl">CONTACT US</Text>
-                <Text>Let&apos;s Start a Conversation</Text>
-                <Text>
-                  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                <Heading size="2xl">CONTACT US</Heading>
+                <Text fontSize="lg" mt={4}>
+                  Cavite Association of Producers & Entrepreneurs - CAPE
+                </Text>
+                <Text mt={4}>
+                  <b>Address:</b> InBox FoodHub, Bayan Luma 8, Emilio Aguinaldo Hi-way, City of Imus <br />
+                  <b>Mobile:</b> +63936 9940234 <br />
+                  <b>Email:</b> capeest.2021@gmail.com <br />
+                  <b>Facebook:</b>{' '}
+                  <Link href="https://www.facebook.com/profile.php?id=100065009122013" color="blue.500" isExternal>
+                    CAPE on Facebook
+                  </Link>
+                </Text>
+                <Text mt={4} fontSize="lg">
+                  Let&apos;s Start a Conversation
                 </Text>
               </Box>
 
               {/* Form */}
-              <Box ml={8} mr={8} color="#0B0E3F">
+              <Box mt={8} ml={8} mr={8} color="#0B0E3F">
                 <VStack spacing={5}>
                   <FormControl id="name">
                     <FormLabel>Your Name</FormLabel>
@@ -62,16 +76,16 @@ const ContactPage = () => {
                       <InputLeftElement pointerEvents="none">
                         <BsPerson color="gray.800" />
                       </InputLeftElement>
-                      <Input type="text" size="md" />
+                      <Input type="text" size="md" placeholder="Enter your name" />
                     </InputGroup>
                   </FormControl>
                   <FormControl id="email">
-                    <FormLabel>Mail</FormLabel>
+                    <FormLabel>Email</FormLabel>
                     <InputGroup borderColor="#E0E1E7">
                       <InputLeftElement pointerEvents="none">
                         <MdOutlineEmail color="gray.800" />
                       </InputLeftElement>
-                      <Input type="email" size="md" />
+                      <Input type="email" size="md" placeholder="Enter your email" />
                     </InputGroup>
                   </FormControl>
                   <FormControl id="message">
@@ -96,8 +110,8 @@ const ContactPage = () => {
             </Box>
           </Box>
         </Container>
-        <Footer />
-      </ChakraProvider>
+       
+      </ChakraProvider> <Footer />
     </>
   );
 };
