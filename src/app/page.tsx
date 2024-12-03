80f18c66323aa92857d3c9cd78aff012c143aac9
NGLkoh <nigelkoh03@gmail.com>
2024-12-03 19:42:57 +0800

    Fixed build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect} from 'react'
 export default function Page() {
 
  	useEffect(() => {
-      window.location.href = "http://localhost:3000/homepage";
+      window.location.href = "ttps://markadong-pinoy-1d27f40bbeb6.herokuapp.com/homepage";
 	}, [])
 
   return <h1></h1>
