b2f5fefb39ac1694d73273846df7d008b255811c
NGLkoh <nigelkoh03@gmail.com>
2024-12-04 14:49:48 +0800

    ui changes

--- pages/blog-client/[id]/index.jsx
+++ pages/blog-client/[id]/index.jsx
@@ -3,6 +3,16 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Text, ChakraProvider, Image, Textarea, Avatar, Input, Button, useToast, useMediaQuery } from '@chakra-ui/react'
 import Navbar from '../../../components/nabvar'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  ViberIcon,
+  ViberShareButton
+} from "react-share";
 import Footer from '../../../components/footer'
 import axios from 'axios'
 import io from 'socket.io-client'
@@ -16,6 +26,8 @@ const BlogClient = () => {
   const [template, setTemplateState] = useState([])
   const [comment, setComment] = useState("")
   const [email, setEmail] = useState("")
+  const [domain, setDomain] = useState("")
+  const [share, setOpenShare] = useState(false)
   const [userId, setUserId] = useState()
   const [comments, setComments] = useState([])
   const [profile, setProfile] = useState([])
@@ -41,6 +53,7 @@ const BlogClient = () => {
   }
 
   const fetchIntialBlog = async () => {
+    setDomain(window.location.href)
     const params = window.location.href.split('/')
     const res = await axios.post('/api/template/template', { id: params[4] })
     console.log(res.data.result[0].ids, "userId")
@@ -115,6 +128,7 @@ const BlogClient = () => {
           <Box padding={ isLargerThan980 ? 20 : 5} position={'relative'} paddingLeft={isLargerThan980? '200px': ""} paddingRight={isLargerThan980? '200px': ""}>
              <Box position={'relative'}>
            <Text float={'left'}   fontSize={18} mb={2}> Comment </Text> 
+         <Box position={'relative'}>
            <Image
              float={'right'}
               alt={'Hero Image'}
@@ -124,11 +138,48 @@ const BlogClient = () => {
               w={'25px'}
               mr={'20px'}
               h={'25px'}
+          
               src={
                 '/like.png'
               }
             />
-
+          <Box position={'absolute'} right={10} display={share ? 'block': 'none'} bottom={0} p={2} width={'200px'}  >
+            <Box position={'relative'} w={'80%'} borderRadius={8} bg={'#232536'} border={'1px solid #dddd'}  p={1} margin={'auto'}>
+	        <Box ml={2} mt={2} display={'inline-block'}>
+            <FacebookShareButton
+				url={domain}
+				quote={`Please like and Share`}
+				hashtag="#MarkadongPinoy">
+				<FacebookIcon logoFillColor="white" size={25} round/>
+			</FacebookShareButton>
+            </Box>
+              <Box ml={2} display={'inline-block'}>
+            <WhatsappShareButton 
+                url={domain}
+                quote={`Please like and Share`}
+                hashtag="#MarkadongPinoy">
+            <WhatsappIcon logoFillColor="white" size={25} round/>
+           </WhatsappShareButton>
+			</Box>
+			<Box ml={2} display={'inline-block'}>
+             <TwitterShareButton 
+                url={domain}
+                quote={`Please like and Share`}
+                hashtag="#MarkadongPinoy">
+            <TwitterIcon logoFillColor="white" size={25} round/>
+           </TwitterShareButton>
+              </Box>
+            <Box ml={2} display={'inline-block'}>
+            <ViberShareButton 
+                url={domain}
+                quote={`Please like and Share`}
+                hashtag="#MarkadongPinoy">
+            <ViberIcon logoFillColor="white" size={25} round/>
+           </ViberShareButton>
+           </Box>
+          </Box>
+        </Box>
+          </Box>
 
             <Image
               alt={'Hero Image'}
@@ -139,6 +190,7 @@ const BlogClient = () => {
               w={'25px'}
               mr={'20px'}
               h={'25px'}
+                 onClick={() => share ? setOpenShare(false) : setOpenShare(true)}
               src={
                 '/share.png'
               }
